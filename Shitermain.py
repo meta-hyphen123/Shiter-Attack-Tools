bddd302caae2319bffc470e45d4b0bb7f326262f
Shiter <136706431+meta-hyphen123@users.noreply.github.com>
2024-02-15 10:53:11 +0800

    Update Shitermain.py

--- Shitermain.py
+++ Shitermain.py
@@ -1,87 +1,56 @@
 import os
 import subprocess
 import re
-import tkinter as tk
-from tkinter import filedialog
-from rich import print
-import urllib
-from urllib.parse import urlparse
+import urllib.parse
 import time
-import socket  # Add this import for the socket module
-import sys
-import os
-import time
-from tqdm import tqdm
 import socket
-import random
 from rich import print
-from colorama import Fore, Style  
 from rich.console import Console
-import urllib.parse  # Add this import
-from colorama import Fore, Style
+
+red = "\033[1;31m"
+green = "\033[1;32m"
+yellow = "\033[1;33m"
+blue = "\033[1;34m"
+purple = "\033[1;35m"
+cyan = "\033[1;36m"
+violate = "\033[1;37m"
+nc = "\033[00m"
 
 console = Console()
 
 pathdog = os.path.dirname(os.path.abspath(__file__))
 pathopen = os.path.join(pathdog, '')
 
-text = r'''
+
+def logo_print(total):
+    print(f'''\007
+{yellow}
  ____  _     _ _                 _   _   _             _    
 / ___|| |__ (_) |_ ___ _ __     / \ | |_| |_ __ _  ___| | __
 \___ \| '_ \| | __/ _ \ '__|   / _ \| __| __/ _` |/ __| |/ /
  ___) | | | | | ||  __/ |     / ___ \ |_| || (_| | (__|   < 
-|____/|_| |_|_|\__\___|_|    /_/   \_\__|\__\__,_|\___|_|\_\
-'''
+|____/|_| |_|_|\__\___|_|    /_/   \_\__|\__\__,_|\___|_|\_\ {purple}v2.1
+{cyan} =============================================
+{yellow}|          Install Best Hacking Tool          |
+{cyan} ============================================={nc}
 
-dog = r'''============================================================'''
 
-eee = '''                   Best Hacking Tool'''
-yellow_text = f"[#FFFD55]{text}[/]"
 
-yellow_eee = f"[#FFFD55]{eee}[/]"
+{yellow}  [ 1 ] {green}XSS attack tool{yellow} [ {purple}{total} tools{yellow} ]
+{yellow}  [ 2 ] {green}SQL .
+{yellow}  [ 3 ] {green}sqlmap.
+{yellow}  [ 4 ] {green}ddos
+{yellow}  [ 5 ] {green}attack flood
+{yellow}  [ 6 ] {green}web2attack
+{yellow}  [ 7 ] {green}sniff
+{yellow}  [ 8 ] {green}nmap
+{yellow}  [ x ] {green}exit''')
 
 
 shitermaininput = """
 ┌──(shiter@main)-[~]
 └─$"""
 
-showtext1 = f"""
-    cmd              use cmd here
-    [1]xss           XSS attack tool
-    [2]sql           find sql bug
-    [3]sqlmap        sqlmap tool
-    [4]ddos          some ddos tools
-    [5]attack flood  python flood Dos
-    [6]web2attck
-    [7]sniffer
-    [8]exit
-
-==========================================================="""
-
-dooo = """    input the number of tool you want to use"""
-yellow_do = f"[#FFFD55]{dooo}[/]"
-
-dogg = """==========================================================="""
-showtext2 = ""
-def about_url(url):
-    try:
-        parsed_url = urllib.parse.urlparse(url)
-        host = parsed_url.hostname
-
-        ip = socket.gethostbyname(host)
-
-        if parsed_url.scheme == 'http':
-            port = 80
-        elif parsed_url.scheme == 'https':
-            port = 443
-        else:
-            print("Unsupported URL scheme.")
-            return
-
-        print(f"URL: {url}\nIP Address: {ip}\nPort: {port}")
-
-    except Exception as e:
-        print(f"Error fetching information: {str(e)}")
 
 def run_command(command):
     try:
@@ -91,6 +60,7 @@ def run_command(command):
     except Exception as e:
         return f"Error running command: {str(e)}"
 
+
 def run_python_script(script_path):
     try:
         subprocess.run(["python", script_path], check=True)
@@ -99,70 +69,48 @@ def run_python_script(script_path):
     except subprocess.CalledProcessError as e:
         print(f"Error running Python script: {e}")
 
-def run_dogshit(script_path):
-    try:
-        subprocess.run(["python", script_path], check=True)
-    except FileNotFoundError:
-        print(f"Python interpreter not found. Make sure Python is installed.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running Python script: {e}")
 
-def run_python_script11(script_path):
-    try:
-        subprocess.run(["cd", script_path], check=True)
-    except FileNotFoundError:
-        print(f"Python interpreter not found. Make sure Python is installed.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running Python script: {e}")
 def main():
-    tools_folder = "tools"
-    target_path = os.path.join(pathdog, tools_folder)
+    total_tools = 8  # 工具总数
+    logo_print(total_tools)
 
-    console.print(yellow_text)
-    console.print(dog)
-    console.print(yellow_eee)
-    console.print(dog)
-    console.print(showtext1)
-    console.print(yellow_do)
-    console.print(dogg)
-    
-    
     while True:
         user_input = input(shitermaininput)
-        
+
         if not user_input:
             continue
-        if user_input.lower() == '8':
+        if user_input.lower() == 'x':
             break
-            
-            
+
         elif user_input.lower() == '4':
             run_python_script(pathopen + "Tools\\ddos_Open.py")
-            
+        elif user_input.lower() == '8':
+            run_python_script(pathopen + "Tools\\nmap_scan.py")
+
         elif user_input.lower() == '2':
             run_python_script(pathopen + "\\Tools\\sql.py")
-            
+
         elif user_input.lower() == '5':
             run_python_script(pathopen + "\\Tools\\psyflood.py")
-            
+
         elif user_input.lower() == '3':
             run_python_script(pathopen + "\\Tools\\sqlmap open.py")
-            
+
         elif user_input.lower() == '7':
             run_python_script(pathopen + "\\Tools\\Shiter_Snifer.py")
-            
+
         elif user_input.lower() == '1':
             run_python_script(pathopen + "\\Tools\\XSS_Open.py")
-            
+
         elif user_input.lower() == '6':
-            xss_folder_path = os.path.join(pathdog,"Tools\\web2attack-master")
+            xss_folder_path = os.path.join(pathdog, "Tools\\web2attack-master")
             os.chdir(xss_folder_path)
             run_python_script("w2aconsole.py")
-            
+
         elif user_input.lower().startswith('about '):
             url_input = user_input[6:]
             about_url(url_input)
-            
+
         elif user_input.lower().startswith('dos '):
             dos_match = re.match(r'dos\s+(\S+)\s+-p\s+(\d+)', user_input.lower())
             if dos_match:
@@ -173,10 +121,11 @@ def main():
                 console.print(result)
             else:
                 console.print("Invalid dos command format. Example: dos <IP> -p<port>")
-                
+
         else:
             result = run_command(user_input)
             console.print(result)
+
 
 if __name__ == "__main__":
     main()
